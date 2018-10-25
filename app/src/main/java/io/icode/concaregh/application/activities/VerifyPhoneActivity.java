158ea803858699615fdeee0066087acb7d5196dd
iCODE <alisampson2@gmail.com>
2018-10-25 13:25:57 -0700

    ConCare GH App - First Version Release - 20th Oct, 2018 - Google Admob added and version code and PhoneNumberAuthentication added but not implemented

---
+++ app/src/main/java/io/icode/concaregh/application/activities/VerifyPhoneActivity.java
@@ -0,0 +1,15 @@
+package io.icode.concaregh.application.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.icode.concaregh.application.R;
+
+public class VerifyPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone);
+    }
+}
