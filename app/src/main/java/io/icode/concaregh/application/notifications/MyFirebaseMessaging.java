7b972c90437640794d0a455c2cd8cae90aa66a98
iCODE <alisampson2@gmail.com>
2019-01-04 18:49:19 -0800

    ConCare GH App - First Version Release - 20th Oct, 2018 - Google Admob added and version code and Sharing Intent added, Chatting Session Completed - Last Updated -> 31st December,2018

--- app/src/main/java/io/icode/concaregh/application/notifications/MyFirebaseMessaging.java
+++ app/src/main/java/io/icode/concaregh/application/notifications/MyFirebaseMessaging.java
@@ -1,11 +1,13 @@
 package io.icode.concaregh.application.notifications;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 
@@ -29,13 +31,23 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         if(firebaseUser != null && sent.equals(firebaseUser.getUid())){
-            sendNotification(remoteMessage);
+
+            //checks build version is Oreo(8.0 and above)
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                sendOreoNotification(remoteMessage);
+            }
+            else{
+                // method call to send notification to devices below android Oreo(8.0)
+                sendNotification(remoteMessage);
+            }
+
         }
 
 
     }
 
-    // sending notification to admin when message is sent
+
+    // sending notification to devices with versions below Oreo(android 8.0)
     private void sendNotification(RemoteMessage remoteMessage) {
 
         String user  = remoteMessage.getData().get("user");
@@ -71,6 +83,39 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
         }
 
         nm.notify(i,builder.build());
+
+    }
+
+    // method to send notification to Oreo devices and above
+    private void sendOreoNotification(RemoteMessage remoteMessage) {
+
+        String user  = remoteMessage.getData().get("user");
+        String icon = remoteMessage.getData().get("icon");
+        String title = remoteMessage.getData().get("title");
+        String body = remoteMessage.getData().get("body");
+
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        int j = Integer.parseInt(user.replaceAll("[\\D]",""));
+        Intent intent = new Intent(this,MessageActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("userid",user);
+        intent.putExtras(bundle);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,j,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        OreoNotification oreoNotification = new OreoNotification(this);
+        Notification.Builder builder = oreoNotification.getOreoNotification(title,body,
+                pendingIntent,defaultSound,icon);
+
+        int i = 0;
+        if(j > 0){
+            i = j;
+        }
+
+        // building notification using notification manager
+        oreoNotification.getManager().notify(i,builder.build());
 
     }
 }
---
+++ app/src/main/java/io/icode/concaregh/application/notifications/OreoNotification.java
@@ -0,0 +1,75 @@
+package io.icode.concaregh.application.notifications;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.net.Uri;
+import android.os.Build;
+
+public class OreoNotification extends ContextWrapper{
+
+    private static final String CHANNEL_ID = "io.icode.concaregh.application";
+
+    private static final String CHANNEL_NAME = "concareghapp";
+
+    private NotificationManager notificationManager;
+
+    public OreoNotification(Context base) {
+        super(base);
+
+        /*check build version of device sending the notification
+         and the device to sent the notification to(target device)*/
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            // method call to create notification channel
+            createChannel();
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    // method to create notification channel
+    private void createChannel() {
+
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT);
+
+        channel.enableLights(false);
+        channel.enableVibration(true);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        // creating notificationChannel with the help of the NotificationManager
+        getManager().createNotificationChannel(channel);
+    }
+
+    // method to create notification manager
+    public NotificationManager getManager(){
+
+        // check if notificationManager is null
+        if(notificationManager == null){
+            notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        // return notificationManager
+        return notificationManager;
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    // method to create notification channel
+    public Notification.Builder getOreoNotification(String title, String body,
+                                                    PendingIntent pendingIntent, Uri soundUri, String icon){
+        // return new Notification.Builder object
+        return new Notification.Builder(getApplicationContext(),CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentIntent(pendingIntent)
+                .setSound(soundUri)
+                .setSmallIcon(Integer.parseInt(icon))
+                .setAutoCancel(true);
+
+    }
+
+}
